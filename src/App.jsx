8df2ab9635063b1e9d71333fa522f849153245f8
Arpita-cloud-cyber <kumariarpita1701@gmail.com>
2025-11-09 03:37:58 +0530

    adding informations

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Arpita Kumari",
+  title: "Information Technology Fresher",
+  email: "officialarpita7595@gmail.com",
+  gitHub: "arpita-cloud-cyber",
+  instagram: "arpita__choudhary02",
+  linkedIn: "-arpita-kumari-",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Motivated and enthusiastic recent graduate seeking an entry-level position where I can apply my academic knowledge, passion for learning, and strong problem-solving skills to contribute to the growth of the organization.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
+  "HTML",
+  "CSS",
+  "JavaScript",
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Git & GitHub",
+  "C & C++",
 ];
 
 /**
