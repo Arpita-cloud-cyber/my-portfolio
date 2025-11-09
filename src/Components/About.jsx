50611b0eb9940e44136a9d51fcff9f6958b47d4b
Arpita Kumari <kumariarpita1701@gmail.com>
2025-11-09 15:47:39 +0000

    update description

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,20 +24,13 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a Motivated and enthusiastic recent graduate seeking an entry-level position where I can apply my academic knowledge, passion for learning, and strong problem-solving skills to contribute to the growth of the organization.";
+  "I'm a motivated and enthusiastic recent graduate seeking an entry-level position where I can apply my academic knowledge, passion for learning, and strong problem-solving skills to contribute to the growth of the organization.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "Web design",
-  "Git & GitHub",
-  "C & C++",
-];
+const skillsList = ["HTML", "CSS", "JavaScript", "Web design", "Git & GitHub", "C & C++"];
 
 /**
  * Use this to give more information about what you are passionate about,
